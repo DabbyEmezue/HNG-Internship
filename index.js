677959ed9d4d4e9f507fe1b71067cfc51ff93e60
Dabby <emezued@example.com>
2022-11-01 11:52:59 -0700

    first

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const port = process.env.PORT || 9000
 
 
 app.get('/', (req,res)=> {
-    res.send('Hello Worlld')
+    res.status(200).json({status: 'success', data: {"Dabby": String, "backend": True, "age": 22, "bio": "My name is Dabby and I run the Back-End track of HNG Internship. I am really glad to be a part of it and I am looking forward to this potentially life changing opportunity." }})
 })
 console.log('hello')
 app.listen(port, ()=>{console.log(`App listening at http://localhost:${port}`)})
-
